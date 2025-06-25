7d3c12293f2f8e8b8be03768a3680fbc7a7ff988
DuarteAd <adrien.duarte65@laposte.net>
2025-06-25 02:09:13 +0200

    chore : ajout de keybinds config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local builtin = require("telescope.builtin")
+
+-- Trouver des fichiers dans ~/.config/nvim (Neovim config)
+map("n", "<leader>fc", function()
+  builtin.find_files({
+    prompt_title = "~ dotfiles ~",
+    cwd = vim.fn.expand("~/.config"),
+    hidden = true,
+    no_ignore = true,
+  })
+end, { desc = "Telescope: dotfiles" })
