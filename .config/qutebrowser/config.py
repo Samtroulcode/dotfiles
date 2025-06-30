726379867745f9cba0e74eb73afb0d07b934fa03
DuarteAd <adrien.duarte65@laposte.net>
2025-06-30 03:23:19 +0200

    feat : privacy settings improved

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -21,7 +21,7 @@ c.url.start_pages = ['http://sam-portal/board']
 
 # url de recherches
 c.url.searchengines = {
-    "DEFAULT": "https://searx.ox2.fr/?q={}", 
+    "DEFAULT": "https://leta.mullvad.net/search?q={}&engine=brave", 
     "wa": "https://wiki.archlinux.org/?search={}",
     "pkg": "https://archlinux.org/packages/?sort=&q={}&maintainer=&flagged=",
     "yt": "https://www.youtube.com/results?search_query={}",
@@ -47,9 +47,11 @@ c.colors.webpage.darkmode.policy.images = 'never'
 config.set("content.webgl", False, "*")
 config.set("content.canvas_reading", False)
 config.set("content.geolocation", False)
-config.set("content.webrtc_ip_handling_policy", "default-public-interface-only")
+config.set("content.webrtc_ip_handling_policy", "disable_non_proxied_udp")
 config.set("content.cookies.store", True)
 config.set("content.cookies.accept", "no-3rdparty")
+config.set("content.headers.accept_language", "en-US,en;q=0.5")
+config.set("content.headers.referer", "no-referrer-when-downgrade")
 
 c.content.blocking.enabled = True
 
