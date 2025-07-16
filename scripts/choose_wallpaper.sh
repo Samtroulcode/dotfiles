4ae68986573375383727420ca64106197e5e95aa
DuarteAd <adrien.duarte65@laposte.net>
2025-07-17 00:34:26 +0200

    fix : removing scripts useless

--- scripts/choose_wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WALLDIR="$HOME/wallpapers"
-
-nsxiv -t "$WALLDIR" | while read -r selected; do
-  [[ -f "$selected" ]] || exit 1
-  $HOME/.config/wallust/set_wall.sh "$selected"
-done
