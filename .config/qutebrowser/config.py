631d1c37b54149b3f82d0a48c55ded9d85b91376
DuarteAd <adrien.duarte65@laposte.net>
2025-07-22 17:09:27 +0200

    feat : webrtc deactivate

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -5,10 +5,7 @@ config.load_autoconfig(False)
 # passe par le proxy microsock qui est connecter via veth au namespace vpn
 config.set('content.proxy', 'socks://10.10.10.2:1080')
 # Empeche WebRTC d'utiliser n'importe quelle ip non lié au proxy
-config.set('content.webrtc_ip_handling_policy', 'disable-non-proxied-udp')
-
-# Appliquer css
-# c.content.user_stylesheets = ['~/.config/qutebrowser/user-styles.css']
+config.set('qt.args',['force-webrtc-ip-handling-policy=disable_non_proxied_udp'])
 
 # Tabs stylisées et plus visibles
 c.tabs.padding = {'top': 8, 'bottom': 8, 'left': 12, 'right': 12}
@@ -19,10 +16,17 @@ c.completion.open_categories = ['searchengines', 'quickmarks', 'bookmarks', 'his
 
 c.auto_save.session = False
 
-c.content.headers.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0'
+c.content.headers.user_agent = (
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
+)
+c.content.headers.custom = {
+    "Sec-CH-UA": '"Chromium";v="132", "Not A;Brand";v="99"',
+    "Sec-CH-UA-Platform": '"Linux"',
+    "Sec-CH-UA-Mobile": '?0',
+}
 c.content.headers.accept_language = "en-US,en;q=0.5"
 c.content.canvas_reading = False
-c.content.webrtc_ip_handling_policy = "default-public-interface-only"
 
 # url de start page
 c.url.start_pages = ['http://sam-portal/board']
@@ -56,7 +60,6 @@ c.colors.webpage.darkmode.policy.images = 'never'
 config.set("content.webgl", False, "*")
 config.set("content.canvas_reading", False)
 config.set("content.geolocation", False)
-config.set("content.webrtc_ip_handling_policy", "default-public-interface-only")
 config.set("content.cookies.store", True)
 config.set("content.cookies.accept", "no-3rdparty")
 config.set("content.headers.accept_language", "en-US,en;q=0.5")
