642aba3fa2148f4fcbb69fc48ebce392d142fc76
DuarteAd <adrien.duarte65@laposte.net>
2025-07-25 11:53:49 +0200

    chore : scripts to create file sys info

---
+++ scripts/sysinfo-refresh.sh
@@ -0,0 +1,14 @@
+# ~/.local/bin/sysinfo-refresh
+#!/usr/bin/env bash
+{
+  echo "###  Infos système (auto)  ###"
+  echo "Hostname: $(hostname)"
+  echo "Kernel: $(uname -r)"
+  echo "CPU: $(lscpu | grep 'Model name' | sed 's/^[^:]*: //')"
+  echo "GPU: $(lspci | grep -E 'VGA|3D' | cut -d':' -f3-)"
+  echo "RAM: $(free -h --si | awk '/Mem:/ {print $2}')"
+  echo "Hyprland: $(hyprctl version | head -1)"
+  echo "Shell: $SHELL"
+  echo "-----------------------------"
+  env | grep -E 'LANG|TERM|EDITOR|VISUAL'
+} >/tmp/sysinfo.txt
