76e4576bb8b5b5f68fe14804c877364c362f76e8
DuarteAd <adrien.duarte65@laposte.net>
2025-08-02 14:48:31 +0200

    feat : Adding a new clean nvim config
    Ajout d'une config "vanilla" de nvim pour tester et tenté de se faire
    une config custom sans distribution

--- .zsh/nvim.zsh
+++ .zsh/nvim.zsh
@@ -3,9 +3,10 @@ alias v-lazy="NVIM_APPNAME=LazyVim nvim"
 alias v-kick="NVIM_APPNAME=kickstart nvim"
 alias v-chad="NVIM_APPNAME=NvChad nvim"
 alias v-astro="NVIM_APPNAME=AstroNvim nvim"
+alias v-nebu="NVIM_APPNAME=NebuVim nvim"
 
 function vs() {
-  items=("default" "kickstart" "LazyVim" "NvChad" "AstroNvim")
+  items=("default" "kickstart" "LazyVim" "NvChad" "AstroNvim" "NebuVim")
   config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
   if [[ -z $config ]]; then
     echo "Nothing selected"
