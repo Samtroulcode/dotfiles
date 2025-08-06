995c4d314cc9aa0cc2734286fa1a952c6d0a6492
DuarteAd <adrien.duarte65@laposte.net>
2025-08-06 13:15:58 +0200

    fix : lisibility updated
    amélioration de la lisibilité des commentaires dans diagnostic.lua

--- .config/nvim/lua/plugins/diagnostics.lua
+++ .config/nvim/lua/plugins/diagnostics.lua
@@ -3,9 +3,9 @@ return {
   opts = {
     -- tous les réglages supportés par vim.diagnostic.config()
     diagnostics = {
-      virtual_text = false, -- 🚫 pas de texte inline
-      underline = true, -- ✔️ gardons le soulignement
-      signs = true, -- ✔️ gardons les pictos dans la colonne
+      virtual_text = false, -- pas de texte inline
+      underline = true, -- gardons le soulignement
+      signs = true, -- gardons les pictos dans la colonne
       update_in_insert = false, -- évite le clignotement en mode Insert
       severity_sort = true, -- tri: erreurs > warnings > infos
     },
