45ca28ad2b4463d098ebbc7bff5dfe2a59faaf9c
DuarteAd <adrien.duarte65@laposte.net>
2025-08-07 03:06:51 +0200

    feat : keybind for cargo watch + copilot
    j'ai rajouté des keybinds pour ouvrir un vsplit avec un cargo watch
    dedans, et pour activer/désactiver copilot

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -14,3 +14,12 @@ map("n", "<leader>fc", function()
     no_ignore = true,
   })
 end, { desc = "Telescope: dotfiles" })
+
+-- Afficher un terminal avec cargo watch qui tourne dedans
+map("n", "<leader>ct", function()
+  vim.cmd('80vsplit | terminal cargo watch -q -c -x "run"')
+end, { desc = "Cargo run watch", noremap = true, silent = true })
+
+-- Active/désactive le mode "Copilot"
+map("n", "<leader>at", "<cmd>Copilot disable<CR>", { desc = "Disable Copilot" })
+map("n", "<leader>aT", "<cmd>Copilot enable<CR>", { desc = "Enable Copilot" })
