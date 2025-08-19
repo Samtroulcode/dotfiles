88dc3f214b0245ad86287f96f3d26c4e5e4fc8b5
DuarteAd <adrien.duarte65@laposte.net>
2025-08-19 06:03:38 +0200

    migrate: switch from bare repo (/home/sam) to normal repo at ~/dotfiles (GNU Stow layout)

--- scripts/app-defaut.sh
+++
@@ -1,6 +0,0 @@
-echo "Explorateur de fichiers : $(xdg-mime query default inode/directory)"
-echo "Navigateur HTTP : $(xdg-mime query default x-scheme-handler/http)"
-echo "Navigateur HTTPS : $(xdg-mime query default x-scheme-handler/https)"
-echo "Lecteur d'images : $(xdg-mime query default image/png)"
-echo "Lecteur vidéo : $(xdg-mime query default video/mp4)"
-
--- scripts/start-stop_waybar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Vérifie si le service Waybar est actif
-if systemctl --user is-active --quiet waybar.service; then
-  systemctl --user stop --now waybar.service
-else
-  systemctl --user start --now waybar.service
-fi
