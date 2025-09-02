13276aa3c7bbb39f65c40104ea42436b9a12bb7e
DuarteAd <adrien.duarte65@laposte.net>
2025-09-02 02:56:12 +0200

    feat : adding fzf multi pickers for movies on servarr
    ajout d'un picker fzf pour regarder des films en sftp avec mpv

--- nebu-zsh/.zsh/nvim.zsh
+++ nebu-zsh/.zsh/nvim.zsh
@@ -3,10 +3,10 @@ alias v-lazy="NVIM_APPNAME=LazyVim nvim"
 alias v-kick="NVIM_APPNAME=kickstart nvim"
 alias v-chad="NVIM_APPNAME=NvChad nvim"
 alias v-astro="NVIM_APPNAME=AstroNvim nvim"
-alias v-nebu="NVIM_APPNAME=NebuVim nvim"
+alias v-nebu="NVIM_APPNAME=nebuvim nvim"
 
 function vs() {
-  items=("default" "kickstart" "LazyVim" "NvChad" "AstroNvim" "NebuVim")
+  items=("default" "kickstart" "LazyVim" "NvChad" "AstroNvim" "nebuvim")
   config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
   if [[ -z $config ]]; then
     echo "Nothing selected"
--- nebu-zsh/.zshrc
+++ nebu-zsh/.zshrc
@@ -55,6 +55,7 @@ export MANPAGER='nvim +Man!'
 [[ -f ~/.zsh/custom/cleantmp.zsh ]] && source ~/.zsh/custom/cleantmp.zsh # petit clean de logs, tmp et bak
 [[ -f ~/.zsh/custom/detach.zsh ]] && source ~/.zsh/custom/detach.zsh # pour détacher complétement un process du shell
 [[ -f ~/.zsh/custom/tmux.zsh ]] && source ~/.zsh/custom/tmux.zsh # Aliases et fonctions tmux
+[[ -f ~/.zsh/custom/fzfm.zsh ]] && source ~/.zsh/custom/fzfm.zsh # picker fzf pour les films du servarr
 
 # Theme d'autocomplétion zsh
 [[ -f ~/.zsh/themes/drac.zsh ]] && source ~/.zsh/themes/drac.zsh
