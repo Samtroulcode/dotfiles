4d136779026ca9bb87234039fb38f31afcdc3f00
DuarteAd <adrien.duarte65@laposte.net>
2025-09-04 23:11:12 +0200

    fix : remove nebu vim config

--- nebu-nvim/.config/nebuvim/init.lua
+++
@@ -1,9 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "markdown", "text", "gitcommit" },
-  callback = function()
-    vim.opt.spell = true
-    vim.opt.spelllang = { "fr", "en" }
-  end,
-})
