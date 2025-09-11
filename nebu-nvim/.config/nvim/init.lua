92f2175e2a4113484fd7a73a31952ffb6e20c503
DuarteAd <adrien.duarte65@laposte.net>
2025-09-11 22:28:00 +0200

    feat : loader lua bytecode for faster require()

--- nebu-nvim/.config/nvim/init.lua
+++ nebu-nvim/.config/nvim/init.lua
@@ -1,3 +1,8 @@
+-- Loader Lua bytecode (Neovim >= 0.9) : accélère les require()
+pcall(function()
+	vim.loader.enable()
+end)
+
 -- Ma touche leader qui sera SPACE
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
@@ -7,6 +12,9 @@ pcall(require, "luarocks.loader")
 
 -- On charge mes options perso
 require("config.options")
+
+-- UI globale (flottants LSP/diag + highlights)
+require("config.ui")
 
 -- On charge le gestionnaire de paquet lazy
 require("config.lazy")
