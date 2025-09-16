5a85eb21d267b88dfbb1fbf60feea14983d31f4c
DuarteAd <adrien.duarte65@laposte.net>
2025-09-16 07:34:46 +0200

    feat : kickstart conf

--- nebu-zsh/.zsh/nvim.zsh
+++ nebu-zsh/.zsh/nvim.zsh
@@ -6,7 +6,7 @@ alias v-astro="NVIM_APPNAME=AstroNvim nvim"
 alias v-nebu="NVIM_APPNAME=nebuvim nvim"
 
 function vs() {
-  items=("default" "kickstart" "LazyVim" "NvChad" "AstroNvim" "nebuvim")
+  items=("default" "nvim-kickstart" "LazyVim" "NvChad" "AstroNvim" "nebuvim")
   config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
   if [[ -z $config ]]; then
     echo "Nothing selected"
