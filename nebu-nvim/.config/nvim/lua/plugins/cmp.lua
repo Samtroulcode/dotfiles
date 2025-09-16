568b5ee869da8420bc986fdef7e7cbd4bc7ae82c
DuarteAd <adrien.duarte65@laposte.net>
2025-09-16 07:35:03 +0200

    chore : increase numbers of cmp entries

--- nebu-nvim/.config/nvim/lua/plugins/cmp.lua
+++ nebu-nvim/.config/nvim/lua/plugins/cmp.lua
@@ -19,7 +19,7 @@ return {
 		require("luasnip.loaders.from_vscode").lazy_load()
 
 		cmp.setup({
-			performance = { max_view_entries = 5 }, -- cap global de la liste (doc officielle)
+			performance = { max_view_entries = 10 }, -- cap global de la liste (doc officielle)
 			snippet = {
 				expand = function(args)
 					luasnip.lsp_expand(args.body)
