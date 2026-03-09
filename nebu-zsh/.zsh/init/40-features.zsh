1548ef184d71fba3303b269bae17d06e2dc30c43
DuarteAd <adrien.duarte65@laposte.net>
2026-03-09 01:33:50 +0100

    feat(zsh): yazi integration

--- nebu-zsh/.zsh/init/40-features.zsh
+++ nebu-zsh/.zsh/init/40-features.zsh
@@ -5,7 +5,6 @@
 [[ -r "$HOME/.zsh/snapper.zsh" ]] && source "$HOME/.zsh/snapper.zsh"
 [[ -r "$HOME/.zsh/nvim.zsh" ]] && source "$HOME/.zsh/nvim.zsh"
 
-[[ -r "$HOME/.zsh/custom/spf.zsh" ]] && source "$HOME/.zsh/custom/spf.zsh"
 [[ -r "$HOME/.zsh/custom/cleantmp.zsh" ]] && source "$HOME/.zsh/custom/cleantmp.zsh"
 [[ -r "$HOME/.zsh/custom/detach.zsh" ]] && source "$HOME/.zsh/custom/detach.zsh"
 [[ -r "$HOME/.zsh/custom/tmux.zsh" ]] && source "$HOME/.zsh/custom/tmux.zsh"
@@ -13,3 +12,5 @@
 [[ -r "$HOME/.zsh/fzf/fzf-movie.zsh" ]] && source "$HOME/.zsh/fzf/fzf-movie.zsh"
 [[ -r "$HOME/.zsh/fzf/fzf-aliases.zsh" ]] && source "$HOME/.zsh/fzf/fzf-aliases.zsh"
 [[ -r "$HOME/.zsh/fzf/fzf-files.zsh" ]] && source "$HOME/.zsh/fzf/fzf-files.zsh"
+
+[[ -r "$HOME/.zsh/custom/yazi.zsh" ]] && source "$HOME/.zsh/custom/yazi.zsh"
